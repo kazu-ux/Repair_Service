a26ae3f4fd5317fbc7f503fc6272c79b2a95ecd2
kazu-ux <61073127+kazu-ux@users.noreply.github.com>
2021-01-20 20:11:47 +0900

    master

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-$('select').change(function () {
-    var val = $('select option:selected').val();
-    if (val == 'select') return;
-    $('section').fadeOut();
-    $('section#' + val ).fadeIn();
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("g700_g700s").style.display = "none"
+});
+
+function selectboxChange() {
+  document.getElementById("g700_g700s").style.display = ""
+  console.log("change")
+  
+}
